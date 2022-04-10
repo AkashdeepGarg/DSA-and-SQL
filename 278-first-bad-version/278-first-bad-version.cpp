5fe80a099186f29a1b517b025859a7c552716f39
AkashdeepGarg <82059488+AkashdeepGarg@users.noreply.github.com>
2022-04-10 23:37:22 +0530

    Time: 0 ms (100.00%), Space: 5.7 MB (89.76%) - LeetHub

---
+++ 278-first-bad-version/278-first-bad-version.cpp
@@ -0,0 +1,18 @@
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        int s = 1,e=n,mid;
+        while(s<=e){
+            mid = s+ (e-s)/2;
+            if(!isBadVersion(mid)){
+                s = mid+1;
+            }
+            else
+                e=mid-1;
+        }
+        return s;   
+    }
+};
