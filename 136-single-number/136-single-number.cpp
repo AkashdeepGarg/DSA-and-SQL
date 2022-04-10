f47b15f9ebeafea6a60f7e9bf206d7134ae4582d
AkashdeepGarg <82059488+AkashdeepGarg@users.noreply.github.com>
2022-04-10 20:07:59 +0530

    Time: 20 ms (79.26%), Space: 16.8 MB (98.83%) - LeetHub

---
+++ 136-single-number/136-single-number.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int n = nums.size();
+        int j = 0;
+        for(int i=0;i<n;i++){
+            j = j xor nums[i];
+        }
+        return j;
+    }
+};
