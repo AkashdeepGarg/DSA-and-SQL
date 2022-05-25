bcfefeb9ebce08abe83fa244d96bd7e51e21d376
AkashdeepGarg <82059488+AkashdeepGarg@users.noreply.github.com>
2022-05-25 16:27:55 +0530

    Time: 17 ms (69.39%), Space: 9.1 MB (60.64%) - LeetHub

--- 494-target-sum/494-target-sum.cpp
+++ 494-target-sum/494-target-sum.cpp
@@ -41,25 +41,34 @@ public:
     //     }
     // }
     int findTargetSumWays(vector<int>& nums, int target) {
-        int n = nums.size();
-        int sm=0;
-        for(auto x : nums)  sm+=x;
-        if(sm<target || (sm-target)%2!=0)
+        int sum=0;
+        int n=nums.size();
+        
+        for(int i=0;i<n;i++)
+            sum+=nums[i];
+        
+        if(sum<target || ((sum+target)/2)<0 || (sum+target)%2!=0)
             return 0;
-        int sm2 = (sm-target)/2;
-        vector<vector<int>> t(n+1,vector<int>(sm2+1,0));
-        t[0][0]=1;
+        
+        sum+=target;
+        sum=sum/2;
+        int dp[n+1][sum+1];
+        
+        dp[0][0]=1;
+        
+        for(int i=1;i<=sum;i++)
+            dp[0][i]=0;
+        
         for(int i=1;i<=n;i++)
-        {
-            for(int j=0;j<=sm2;j++)
-            {
-                if(nums[i-1]<=j)
-                    t[i][j] = t[i-1][j] + t[i-1][j-nums[i-1]];
-                else
-                    t[i][j] = t[i-1][j];
+            dp[i][0]=1;
+        
+        for(int i=1;i<=n;i++){
+            for(int j=0;j<=sum;j++){
+                if(nums[i-1]<=j){
+                    dp[i][j]=dp[i-1][j-nums[i-1]]+dp[i-1][j];
+                }else dp[i][j]=dp[i-1][j];
             }
         }
-        return t[n][sm2];
-        
+        return dp[n][sum];
     }
 };
