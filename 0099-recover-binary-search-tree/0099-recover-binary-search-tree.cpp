01cfbd59078f1eb440b385d545db1232f58c0503
AkashdeepGarg <82059488+AkashdeepGarg@users.noreply.github.com>
2024-03-17 17:14:14 +0530

    Time: 28 ms (22.14%), Space: 60.4 MB (64.23%) - LeetHub

--- 0099-recover-binary-search-tree/0099-recover-binary-search-tree.cpp
+++ 0099-recover-binary-search-tree/0099-recover-binary-search-tree.cpp
@@ -10,52 +10,93 @@
  * };
  */
 class Solution {
-public:
-    void correct(TreeNode* root,int first,int last){
-        if(!root){
-            return;
-        }
-        if(root->val==first){
-            root->val=last;
-        }
-        else if(root->val==last){
-            root->val=first;
-        }
-        correct(root->left,first,last);
-        correct(root->right,first,last);
-    }
-    void inorder(TreeNode* root,vector<int> &nums){
-        if(!root){
-            return;
-        }
-        if(root->left){
-            inorder(root->left,nums);
-        }
-        nums.push_back(root->val);
-        if(root->right){
-            inorder(root->right,nums);
-        }
-    }
-    void recoverTree(TreeNode* root) {
-        if(!root){
-            return;
-        }
-        int first,last;
-        int flag = 0;
-        vector<int> nums;
-        inorder(root,nums);
-        vector<int> copy = nums;
-        sort(copy.begin(),copy.end());
-        for(int i=0;i<nums.size();i++){
-            if(copy[i]!=nums[i]){
-                if(flag==0){
-                    first = copy[i];
-                    flag=1;
-                }
-                last = copy[i];
+// public:
+    // void correct(TreeNode* root,int first,int last){
+    //     if(!root){
+    //         return;
+    //     }
+    //     if(root->val==first){
+    //         root->val=last;
+    //     }
+    //     else if(root->val==last){
+    //         root->val=first;
+    //     }
+    //     correct(root->left,first,last);
+    //     correct(root->right,first,last);
+    // }
+    // void inorder(TreeNode* root,vector<int> &nums){
+    //     if(!root){
+    //         return;
+    //     }
+    //     if(root->left){
+    //         inorder(root->left,nums);
+    //     }
+    //     nums.push_back(root->val);
+    //     if(root->right){
+    //         inorder(root->right,nums);
+    //     }
+    // }
+    // void recoverTree(TreeNode* root) {
+    //     if(!root){
+    //         return;
+    //     }
+    //     int first,last;
+    //     int flag = 0;
+    //     vector<int> nums;
+    //     inorder(root,nums);
+    //     vector<int> copy = nums;
+    //     sort(copy.begin(),copy.end());
+    //     for(int i=0;i<nums.size();i++){
+    //         if(copy[i]!=nums[i]){
+    //             if(flag==0){
+    //                 first = copy[i];
+    //                 flag=1;
+    //             }
+    //             last = copy[i];
+    //         }
+    //     }
+    //     correct(root,first,last);
+    //     return;
+    // }
+    
+    
+    private: 
+    TreeNode* first;
+    TreeNode* prev;
+    TreeNode* middle;
+    TreeNode* last; 
+private: 
+    void inorder(TreeNode* root) {
+        if(root == NULL) return; 
+        
+        inorder(root->left);
+        
+        if (root->val < prev->val)
+        {
+           
+            // If this is first violation, mark these two nodes as
+            // 'first' and 'middle'
+            if ( first == NULL )
+            {
+                first = prev;
+                middle = root;
             }
+ 
+            // If this is second violation, mark this node as last
+            else
+                last = root;
         }
-        correct(root,first,last);
-        return;
+ 
+        // Mark this node as previous
+        prev = root;
+        inorder(root->right); 
+    }
+public:
+    void recoverTree(TreeNode* root) {
+        first = middle = last = NULL; 
+        prev = new TreeNode(INT_MIN); 
+        inorder(root);
+        if(first && last) swap(first->val, last->val); 
+        else if(first && middle) swap(first->val, middle->val); 
     }
 };
