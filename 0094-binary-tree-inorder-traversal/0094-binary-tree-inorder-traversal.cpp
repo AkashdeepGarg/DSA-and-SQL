38763ddddb7497ddaa213b1b1376d721a2810960
AkashdeepGarg <82059488+AkashdeepGarg@users.noreply.github.com>
2024-03-17 14:56:30 +0530

    Time: 4 ms (31.68%), Space: 9.9 MB (37.12%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -11,22 +11,49 @@
  */
 class Solution {
 public:
-    void solve(TreeNode* root, vector<int> &ans){
-        if(root->left){
-            solve(root->left,ans);
-        }
-        ans.push_back(root->val);
-        if(root->right){
-            solve(root->right,ans);
-        }
-    }
+    // First Solution - 
+//     void solve(TreeNode* root, vector<int> &ans){
+//         if(root->left){
+//             solve(root->left,ans);
+//         }
+//         ans.push_back(root->val);
+//         if(root->right){
+//             solve(root->right,ans);
+//         }
+//     }
     
+//     vector<int> inorderTraversal(TreeNode* root) {
+//         vector<int> ans;
+//         if(!root){
+//             return ans;
+//         }
+//         solve(root,ans);
+//         return ans;
+//     }
+    
+    // SECOND solution
     vector<int> inorderTraversal(TreeNode* root) {
         vector<int> ans;
         if(!root){
             return ans;
         }
-        solve(root,ans);
+        TreeNode* temp = root;
+        stack<TreeNode*> s;
+        while(true){
+            if(temp){
+                s.push(temp);
+                temp = temp->left;
+            }
+            else{
+                if(s.empty()){
+                    return ans;
+                }
+                temp = s.top();
+                ans.push_back(temp->val);
+                s.pop();
+                temp = temp->right;
+            }
+        }
         return ans;
     }
 };
