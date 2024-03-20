10cfe8bfa4c590cd1d5e3e0f0e4e79aad05f45d7
AkashdeepGarg <82059488+AkashdeepGarg@users.noreply.github.com>
2024-03-21 03:19:10 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (77.09%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,10 +1,22 @@
 class Solution {
 public:
+    // Using Log Properties
+    //  bool isPowerOfTwo(int n) {
+    //     if(n <= 0) return false;
+    //     return ceil(log2(n)) == floor(log2(n));
+    // }
+    
+    //Using bit manipulation
     bool isPowerOfTwo(int n) {
-        if(n==0){
-            return false;
-        }
-        while(n%2==0) n/=2;
-        return n==1;
+        if(n<=0) return false;
+        return ((n&(n-1))==0);
     }
+
+    // bool isPowerOfTwo(int n) {
+    //     if(n==0){
+    //         return false;
+    //     }
+    //     while(n%2==0) n/=2;
+    //     return n==1;
+    // }
 };
