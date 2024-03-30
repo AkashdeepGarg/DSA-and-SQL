970a37cff7803ee0fcaf90df0ae2dc1294243272
AkashdeepGarg <82059488+AkashdeepGarg@users.noreply.github.com>
2024-03-31 02:16:04 +0530

    Time: 99 ms (74.83%), Space: 49.7 MB (56.74%) - LeetHub

--- 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.cpp
+++ 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.cpp
@@ -1,32 +1,25 @@
 class Solution {
 public:
-    int subarraysWithKDistinct(vector<int>& nums, int k) {
-        return slidingWindowAtMost(nums, k) - slidingWindowAtMost(nums, k - 1);
-    }
-
-private:
-    // Helper function to count the number of subarrays with at most k distinct elements.
-    int slidingWindowAtMost(vector<int>& nums, int distinctK) {
-        // To store the occurrences of each element.
-        map<int, int> freqMap;
-        int left = 0, totalCount = 0;
-
-        // Right pointer of the sliding window iterates through the array.
-        for (int right = 0; right < nums.size(); right++) {
-            freqMap[nums[right]]++;
-
-            // If the number of distinct elements in the window exceeds k,
-            // we shrink the window from the left until we have at most k distinct elements.
-            while (freqMap.size() > distinctK) {
-                freqMap[nums[left]]--;
-                if (freqMap[nums[left]] == 0) 
-                    freqMap.erase(nums[left]);
-                left++;
+    int solve(vector<int>& nums, int k){
+        int n = nums.size();
+        unordered_map<int,int> mp;
+        int i=0,j=0,ans=0;
+        while(j<n){
+            mp[nums[j]]++;
+            while(mp.size() > k){
+                mp[nums[i]]--;
+                if(mp[nums[i]]==0){
+                    mp.erase(nums[i]);
+                }
+                i++;
             }
-
-            // Update the total count by adding the length of the current subarray.
-            totalCount += (right - left + 1);
+            ans = ans+j-i+1;
+            j++;
         }
-        return totalCount;
+        
+        return ans;
+    }
+    int subarraysWithKDistinct(vector<int>& nums, int k) {
+        return solve(nums,k) - solve(nums,k-1);
     }
 };
