034be273e5af8f3d440f4b2ca585a34c9b6767ff
AkashdeepGarg <82059488+AkashdeepGarg@users.noreply.github.com>
2024-04-05 17:15:48 +0530

    Time: 3 ms (97.97%), Space: 13.6 MB (99.91%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.cpp
+++ 0740-delete-and-earn/0740-delete-and-earn.cpp
@@ -8,7 +8,15 @@ public:
         int a[m+1],i,n=nums.size();
         for(int &t:a) t=0;
         for(int k : nums)a[k]+=k;
-        for(i=2;i<m+1;i++)a[i]=max(a[i-1],a[i]+a[i-2]);
-        return a[i-1];
+        
+        int inc = a[0];
+        int exc = 0;
+        int ans;
+        for(int i=1;i<=m;i++){
+            ans = max(inc,exc);
+            inc = exc + a[i];
+            exc = ans;
+        }
+        return max(inc,exc);
     }
 };
