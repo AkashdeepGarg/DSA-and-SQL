7ed0fd5baa97b14322207bd2410ac9cf73edb14d
AkashdeepGarg <82059488+AkashdeepGarg@users.noreply.github.com>
2025-01-11 01:09:18 +0530

    Time: 0 ms (100%), Space: 8.9 MB (48.12%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.cpp
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
     int lengthOfLastWord(string s) {
-        int curLen = 0;
-	for (int i = s.length() - 1; i >= 0; i--) {
-		if (s[i] == ' ') {
-			// Answer is the first time curLen>0, 
-			// for it's the first word we met in s from backward.
-			if (curLen > 0)
-				return curLen;
-			continue;
-		}
-		curLen++;
-	}
-	return curLen;
+        reverse(s.begin(),s.end());
+        int ans=0;
+        for(int i=0;i<s.length();i++){
+            if(s[i]==' ' and ans!=0){
+                break;
+            }else if(s[i]==' '){
+                continue;
+            }
+            else{
+                ans++;
+            }
+        }
+        return ans;
     }
 };
