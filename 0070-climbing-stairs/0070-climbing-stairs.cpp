77118f09bc0f8af37fe5b31ec498eebfc056ed76
AkashdeepGarg <82059488+AkashdeepGarg@users.noreply.github.com>
2025-04-04 11:06:48 +0530

    Time: 0 ms (100%), Space: 8.6 MB (17.25%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,38 +1,21 @@
 class Solution {
 public:
-    
-    //Memoization
-    // int climbStairs(int n, vector<int> & t){
-    //     if(n<0){
-    //         return 0;
-    //     }
-    //     if(n==0){
-    //         return 1;
-    //     }
-    //     if(t[n]!=-1){
-    //         return t[n];
-    //     }
-    //     return t[n] = climbStairs(n-1)+climbStairs(n-2);
-    // }
-    int climbStairs(int n) {
-        //Recursion
-        // if(n<0){
-        //     return 0;
-        // }
-        // if(n==0){
-        //     return 1;
-        // }
-        // return climbStairs(n-1)+climbStairs(n-2);
-        
-        //Momoization and DP(Bottom UP approach)
-        
-        vector<int> t(46);
-        t[0] = 1;
-        t[1] = 1;
-        for(int i=2;i<=n;i++){
-            t[i] = t[i-1]+t[i-2];
+    int solve(int n, vector<int>& dp) {
+        if (n == 1 || n == 0) {
+            return 1;
         }
-        return t[n];
-        // return climbStairs(n,t);
+        if(dp[n]!=-1){
+            return dp[n];
+        }
+        return dp[n] = solve(n - 1, dp) + solve(n - 2, dp);
+    }
+    int climbStairs(int n) {
+        if (n == 1 || n == 0) {
+            return 1;
+        }
+        vector<int> dp(n+1, -1);
+        solve(n, dp);
+
+        return dp[n];
     }
 };
