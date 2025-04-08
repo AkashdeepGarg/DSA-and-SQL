3a281143abffbd5ff9a46577b2c5366823dfa355
AkashdeepGarg <82059488+AkashdeepGarg@users.noreply.github.com>
2025-04-08 22:33:41 +0530

    Time: 5 ms (32.06%), Space: 9.8 MB (48.44%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,20 +1,8 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if(s.length()!=t.length()){
-            return false;
-        }
-        int n = t.length();
-        unordered_map<char,int> mp;
-        for(char c:s){
-            mp[c]++;
-        }
-        for(char c:t){
-            if(mp[c]<1){
-                return false;
-            }
-            mp[c]--;
-        }
-        return true;
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        return s==t;
     }
 };
