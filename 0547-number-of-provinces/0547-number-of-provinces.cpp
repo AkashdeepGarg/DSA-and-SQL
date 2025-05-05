773cae299c83c99ce8ad464058811da63a296206
AkashdeepGarg <82059488+AkashdeepGarg@users.noreply.github.com>
2025-05-05 21:23:17 +0530

    Time: 0 ms (100%), Space: 19.5 MB (72.14%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,3 +1,44 @@
+class DisjointSet{
+    public:
+    vector<int> parent,size;
+
+    DisjointSet(int n){
+        parent.resize(n+1);
+        size.resize(n+1,1);
+
+        for(int i=0;i<=n;i++){
+            parent[i]=i;
+        }
+    }
+
+    int findParent(int v){
+        if(parent[v]==v){
+            return v;
+        }
+
+        return parent[v] = findParent(parent[v]);
+    }
+
+    void unionParent(int u, int v){
+        int pu = findParent(u);
+        int pv = findParent(v);
+
+        if(pu==pv){
+            return;
+        }
+
+        if(size[pu]>size[pv]){
+            parent[pv] = pu;
+            size[pu]+=size[pv];
+        }
+        else{
+            parent[pu]=pv;
+            size[pv]+=size[pu];
+        }
+    }
+};
+
+
 class Solution {
 public:
     void dfs(int node, vector<int>& visited, vector<int> graph[]){
@@ -10,26 +51,48 @@ public:
     }
 
     int findCircleNum(vector<vector<int>>& isConnected) {
-        int count=0;
+        //DFS
+        // int count=0;
+        // int n = isConnected.size();
+        // vector<int> graph[n];
+
+        // for(int i=0;i<n;i++){
+        //     for(int j=0;j<n;j++){
+        //         if(isConnected[i][j]==1 && i!=j){
+        //             graph[i].push_back(j);
+        //             graph[j].push_back(i);
+        //         }
+        //     }
+        // }
+
+        // vector<int> visited(n,0);
+        // for(int i=0;i<n;i++){
+        //     if(visited[i]==0){
+        //         count++;
+        //         dfs(i,visited,graph);
+        //     }
+        // }
+        // return count;
+
+
+        //Kruskal's Algo
         int n = isConnected.size();
-        vector<int> graph[n];
+        DisjointSet d(n);
 
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
                 if(isConnected[i][j]==1 && i!=j){
-                    graph[i].push_back(j);
-                    graph[j].push_back(i);
+                    d.unionParent(i,j);
                 }
             }
         }
-
-        vector<int> visited(n,0);
+        int count=0;
         for(int i=0;i<n;i++){
-            if(visited[i]==0){
+            if(d.parent[i]==i){
                 count++;
-                dfs(i,visited,graph);
             }
         }
+
         return count;
     }
 };
