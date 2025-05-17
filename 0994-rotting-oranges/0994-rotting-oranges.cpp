c16f09ea2c9df168701852f680a4178fda34a58c
AkashdeepGarg <82059488+AkashdeepGarg@users.noreply.github.com>
2025-05-17 11:11:53 +0530

    Time: 0 ms (100%), Space: 17 MB (44.94%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -3,44 +3,45 @@ public:
     int orangesRotting(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
-        int ans=0;
-
-        vector<vector<int>> visited(n,vector<int>(m,0));
-        queue<pair<pair<int,int>,int>> q;
+        vector<vector<int>> visited(n,(vector<int>(m,0)));
+        priority_queue<pair<int,pair<int,int>>, vector<pair<int,pair<int,int>>>, greater<pair<int,pair<int,int>>>> pq;
 
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(grid[i][j]==2){
-                    q.push({{i,j},0});
-                    visited[i][j]=2;
+                    visited[i][j]=1;
+                    pq.push({0,{i,j}});
                 }
             }
         }
-        int di[] = {-1,1,0,0};
-        int dj[] = {0,0,1,-1};
+        int di[]={-1,1,0,0};
+        int dj[]={0,0,-1,1};
 
-        while(q.size()>0){
-            int r = q.front().first.first;
-            int c = q.front().first.second;
-            int t = q.front().second;
-            ans = max(ans,t);
-            q.pop();
+        int ans = 0;
 
-            for(int i=0;i<4;i++){
-                int newr = r+di[i];
-                int newc = c+dj[i];
-                if(newr>=0 && newr<n && newc>=0 && newc<m && visited[newr][newc]==0 && grid[newr][newc]==1){
-                    visited[newr][newc]=2;
-                    grid[newr][newc]=2;
-                    q.push({{newr,newc},t+1});
+        while(pq.size()>0){
+            auto t = pq.top();
+            pq.pop();
+            int time = t.first;
+            int i = t.second.first;
+            int j = t.second.second;
+            ans = max(ans,time);
+
+            for(int k=0;k<=3;k++){
+                int row = i + di[k];
+                int col = j + dj[k];
+
+                if(row>=0 && col>=0 && row<n && col<m && grid[row][col]==1 && visited[row][col]==0){
+                    pq.push({time+1,{row,col}});
+                    visited[row][col]=1;
                 }
             }
         }
 
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(grid[i][j]==1){
-                   return -1;
+                if(visited[i][j]==0 && grid[i][j]==1){
+                    return -1;
                 }
             }
         }
